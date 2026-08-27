be2f439ba8506a802cf09a73247b2d9c6a13f767
unknown <galaxyslayer0009@gmail.com>
2026-08-27 02:32:49 -0300

    feat: rotas pra barbeiro agora podem consultar serviços

--- src/rotas/barbeiro.js
+++ src/rotas/barbeiro.js
@@ -2,11 +2,13 @@ const express = require("express");
 const autenticarUsuario = require("../middleware/authMiddleware");
 const { buscarTodosAgendamentos, concluirAgendamento } = require("../controllers/agendamentosController");
 const { buscarFaturamento } = require("../controllers/financeiroController")
+const { buscarServicos } = require("../controllers/servicosController");
 
 const rotasBarbeiro = express.Router();
 
 rotasBarbeiro.get("/agendamentos", autenticarUsuario, buscarTodosAgendamentos);
 rotasBarbeiro.get("/faturamento", autenticarUsuario, buscarFaturamento);
+rotasBarbeiro.get("/servicos", autenticarUsuario, buscarServicos);
 
 rotasBarbeiro.patch("/:agendamento_id/concluirAgendamento", autenticarUsuario, concluirAgendamento);
 
